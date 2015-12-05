adb72c6431f39640c0849c315adeef96bee76008
“ikrikor” <armanie94@icloud.com>
2015-12-05 12:44:08 -0600

    Initial Commit

---
+++ CFG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CFG
+//
+//  Created by Krikor Bisdikian on 12/5/15.
+//  Copyright © 2015 Krikor Bisdikian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
